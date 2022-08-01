ac6ed21a7ec1d04322225c1af8f1ff6a0423e2bc
Avazbek Isroilov <64166521+Avazbek2002@users.noreply.github.com>
2022-08-01 17:23:07 +0500

    loops

--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -2,8 +2,9 @@ package school.mjc.stage0.loops.task4;
 
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
-        for (int i = 1; (i <= lastPrinted) && (i % 3 != 0); i++) {
-            System.out.println(i);
+        for (int i = 1; i <= lastPrinted; i++) {
+            if (i % 3 != 0)
+                System.out.println(i);
         }
     }
 }
